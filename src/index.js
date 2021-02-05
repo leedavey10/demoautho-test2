98ca215a7beed3f5144a147c00051c90a8529217
leedavey10 <78532723+leedavey10@users.noreply.github.com>
2021-02-05 11:12:29 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
   <Auth0Provider
-    domain="leedavey10.eu.auth0.com"
+    domain="https://main.d1zt792rjak84s.amplifyapp.com/"
     clientId="c9ofYYKDCjZHtf4quGqPeZNJXTCA7Em3"
     redirectUri={window.location.origin}
   >
