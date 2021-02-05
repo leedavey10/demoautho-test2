0d7c96f17f990e36e4e61cf2d7835bc93ced0b18
leedavey10 <78532723+leedavey10@users.noreply.github.com>
2021-02-05 10:00:26 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Lee Davey's Web Application 
         </p>
         <a
           className="App-link"
