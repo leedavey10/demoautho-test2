64fe5f3e972aae8bd49d20beea0d875fd19d569a
leedavey10 <78532723+leedavey10@users.noreply.github.com>
2021-02-05 11:38:11 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Auth0Provider
+    domain="leedavey10.eu.auth0.com"
+    clientId="c9ofYYKDCjZHtf4quGqPeZNJXTCA7Em3"
+    redirectUri={window.location.origin}
+  >
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Auth0Provider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
